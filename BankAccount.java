72b05e7118ab04c0cb3482292d7fd6a0c4716a9f
Camila Contreras <ccontreras20@stuy.edu>
2020-10-29 11:07:55 -0700

    All functions written

---
+++ BankAccount.java
@@ -0,0 +1,37 @@
+public class BankAccount {
+  private double balance;
+  private int accountID;
+  private String password;
+  // constructor for accountID and password
+  public BankAccount (int a, String p) {
+    accountID = a;
+    password = p;
+    balance = 0;
+  }
+  public double getBalance() {
+    return balance;
+  }
+  public int getAccountID() {
+    return accountID;
+  }
+  public void setPassword(String pass) {
+    password = pass;
+  }
+  public boolean deposit (double amount) {
+    if (amount >= 0) {
+      balance += amount;
+      return true;
+    }
+    return false;
+  }
+  public boolean withDraw(double amount) {
+    if (amount>=0 && amount <= balance) {
+      balance -= amount;
+      return true;
+    }
+    return false;
+  }
+  public String toString() {
+    return accountID + "\t" + balance;
+  }
+}
